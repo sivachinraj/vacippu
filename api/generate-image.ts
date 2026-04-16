72d09c10bb12c6795b1aa208fc57cdf4a435b84c
Dhanvanth Sivakumar <cooldhan30@gmail.com>
2026-04-15 22:49:43 -0400

    Update ComfyUI tunnel URL

--- api/generate-image.ts
+++ api/generate-image.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-const COMFY_URL = "https://cycles-resist-murray-luther.trycloudflare.com";
+const COMFY_URL = "https://fossil-joining-bulk-nurse.trycloudflare.com";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader("Access-Control-Allow-Origin", "*");
